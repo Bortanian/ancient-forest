3e6a3c1d10463c32a642a85366424938e2c1138a
Erik Amundson <SavenTheChickens@gmail.com>
2018-06-22 14:31:11 -0600

    added styling in battle component

--- src/components/Battle/Battle.js
+++ src/components/Battle/Battle.js
@@ -1,21 +1,39 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import './Battle.css'
 
-export default class Battle extends Component{
-    render(){
-        return(
+export default class Battle extends Component {
+    constructor() {
+        super()
+        this.state = {
+            fightToggle: false
+        }
+    }
+    render() {
+        return (
             <div>
-                BATTLE COMPONENT
-                <div className='battle-view'>
+                <div className='battle-view'
+                    style={{
+                        position: 'relative',
+                        width: '880px',
+                        height: '560px',
+                        margin: '170px auto',
+                        border: '4px solid white',
+                    }}
+                >
                     <main className='action'>
                     </main>
 
-                    <section className='text'>
-                    </section>
+                    <div className='lower-action'>
+                        <section className='text'>
+                        </section>
 
-                    <section className='options'>
-                        <button className='fight'>FIGHT</button>
-                        <button className='run'>RUN</button>
-                    </section>
+                        <section className='options'>
+                            <button className='fight'
+                                onClick={() => this.setState({ fightToggle: !this.state.fightToggle })}
+                                >FIGHT</button>
+                            <button className='run'>RUN</button>
+                        </section>
+                    </div>
                 </div>
             </div>
         )
