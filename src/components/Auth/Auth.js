a9ce94758c1d3d1ed3f6fa7b46447cfa36c91020
Erik Amundson <SavenTheChickens@gmail.com>
2018-07-02 12:13:30 -0600

    presentation ready

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './Auth.css'
 
-export default function Auth(){
-    return(
-        <div className='login-box'>
+export default function Auth() {
+    return (
+        <div>
+            <div className='auth-box'>
+                <h1>WELCOME TO THE ANCIENT FOREST</h1>
             <a href={process.env.REACT_APP_LOGIN}>
-                <h2 className='login'>LOGIN</h2>
+                <div className='login-box'>
+                    <h2 className='login'>LOGIN</h2>
+                </div>
             </a>
-        </div> 
+            </div>
+        </div>
     )
 }
